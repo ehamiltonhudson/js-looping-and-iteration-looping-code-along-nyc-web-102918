340f8cbb86933623685acfa9bd25cfdf734009c2
Hamilton Hudson <ehamiltonhudson@gmail.com>
2018-12-10 06:15:11 -0500

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,19 @@
 // Code your solutions in this file
+
+function printBadges(array) {
+  for (let i = 0; i < array.length; i++) {
+    console.log(`Welcome ${array[i]}! You are employee #${i+1}.`)
+  }
+  return array;
+}
+
+function tailsNeverFails() {
+  function coinToss() {
+    return Math.random() >= 0.5;
+  }
+  let count = 0;
+  while (coinToss()) {
+    count++;
+  }
+  return `You got ${count} tails in a row!`;
+}
